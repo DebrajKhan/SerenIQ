b655684a3efba21353b23e2bc5064c1c18dccd1f
unknown <debrajkhan007@gmail.com>
2026-09-12 13:25:40 +0530

    env file is updated

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     mongodb_url : str
     secret_key : str
-    algorithm : str = "HS256"
-    access_token_time :int =  5
+    algorithm : str 
+    access_token_time :int
 
     class Config:
         env_file = ".env"
