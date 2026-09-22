85f3b48128836bf956107879ba865ffbf6935cd0
unknown <debrajkhan007@gmail.com>
2026-09-23 00:49:42 +0530

    websocket is added for chatroom

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from databases.database import connected_to_mongoDB, terminate_mongoDB
-from routers import auth, add_friend,my_friends,respond_friend_req,get_friend_req
+from routers import auth, add_friend,my_friends,respond_friend_req,get_friend_req, chat_socket
 
 
 
@@ -32,3 +32,4 @@ app.include_router(add_friend.router)
 app.include_router(my_friends.router)
 app.include_router(respond_friend_req.router)
 app.include_router(get_friend_req.router)
+app.include_router(chat_socket.router)
---
+++ backend/routers/chat_socket.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from typing import Dict
+
+router = APIRouter()
+
+class ConnectionManager():
+    def __init__(self):
+        self.active_connections: Dict[str, WebSocket] = {}
+
+    async def connect(self, websocket:WebSocket, email:str):
+        await websocket.accept()
+        self.active_connections[email] = websocket
+
+    def disconnect(self, email:str):
+        if email in self.active_connections:
+            del self.active_connections[email]
+
+    async def send_personal_message(self, message:str, sender_email:str, target_email:str):
+        if target_email in self.active_connections:
+            payload = {"message" : message}
+            self.active_connections[target_email].send_json(payload)
+
+manager = ConnectionManager()
+
+@router.websocket("/ws/chat/{user_email}")
+async def websocket_endpoint(websocket:WebSocket, user_email : str):
+    await manager.connect(websocket, user_email)
+    try:
+        while True:
+            data = await websocket.receive_json()
+            target = data.get("target_email")
+            message = data.get("message")
+            await manager.send_personal_message(message, user_email, target)
+
+    except WebSocketDisconnect:
+        manager.disconnect(user_email)
